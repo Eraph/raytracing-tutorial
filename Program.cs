74a515e04c5efafab07caa0c805c6ac2fd2602e9
Phil Watson <phil@rakhama.com>
2025-12-09 20:18:16 +1000

    First step, coloured square

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿int imageWidth = 256;
+int imageHeight = 256;
+
+Console.WriteLine("P3");
+Console.WriteLine($"{imageWidth} {imageHeight}");
+Console.WriteLine(255);
+
+for (int j = 0; j < imageHeight; j++)
+{
+    for (int i = 0; i < imageWidth; i++)
+    {
+        var r = (double)i / (imageWidth - 1);
+        var g = (double)j / (imageHeight - 1);
+        var b = 0.0;
+
+        int ir = (int)(255.999 * r);
+        int ig = (int)(255.999 * g);
+        int ib = (int)(255.999 * b);
+
+        Console.WriteLine($"{ir} {ig} {ib}");
+    }
+}
---
+++ readme.md
@@ -0,0 +1,10 @@
+# Raytracer
+
+## Introduction
+
+Having a go at [Raytracing In One Weekend](https://raytracing.github.io/books/RayTracingInOneWeekend.html).
+
+## Usage
+``` bash
+dotnet run > image.ppm
+```
